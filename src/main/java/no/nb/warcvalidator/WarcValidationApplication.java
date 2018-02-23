a2a03f054f23ec8692db82baf532f1b4cb8ed316
Andreas Børsheim <andreas.borsheim@nb.no>
2018-02-23 09:38:11 +0100

    fix: Check if directory is empty

--- src/main/java/no/nb/warcvalidator/WarcValidationApplication.java
+++ src/main/java/no/nb/warcvalidator/WarcValidationApplication.java
@@ -26,54 +26,63 @@ public class WarcValidationApplication {
 
             sleepBetweenChecks = Integer.parseInt(args[0]);
 
-            // Sett til persistentVolume som inneholder warc på cluster
-            File contentDirectory = new File("/warcs");
-            String validWarcDirectory = "/validwarcs";
+            // Prod
+            // File contentDirectory = new File("/warcs");
+            // String validWarcDirectory = "/validwarcs";
+
+            // Dev test
+            File contentDirectory = new File("src/Testfiles/warcs");
+            String validWarcDirectory = "src/Testfiles/validwarcs";
             String reportName;
             File[] files = contentDirectory.listFiles();
 
             ValidationService service = new ValidationService();
 
             // Filområde for warc (contentWriter) er ikke tomt
-            while (files != null) {
-                logger.info("Will validate and move WARC files");
-                ArrayList<File> warcs = service.findAllWarcs(files);
-                ArrayList<File> reports = service.findAllReports(files);
+            while (true) {
 
-                // ser gjennom alle  warc-filer
-                for (File warc : warcs) {
-                    String warcFilename = warc.getName();
-                    String warcFilePath = warc.getAbsolutePath();
+                if(files.length > 0) {
+                    logger.info("Will validate and move WARC files");
+                    ArrayList<File> warcs = service.findAllWarcs(files);
+                    ArrayList<File> reports = service.findAllReports(files);
 
-                    // Sjekker om warc-fil allerede ligger i valid katalog
-                    if (service.warcMovedToValid(validWarcDirectory, warcFilename)) {
-                        logger.info(warcFilename + " already validated and moved to final directory");
-                    } else {
+                    // ser gjennom alle  warc-filer
+                    for (File warc : warcs) {
+                        String warcFilename = warc.getName();
+                        String warcFilePath = warc.getAbsolutePath();
 
-                        // Sjekker om warc-fil har en rapport
-                        File validationReport = service.reportForWarcExist(reports, warcFilename);
-                        if (validationReport != null) {
+                        // Sjekker om warc-fil allerede ligger i valid katalog
+                        if (service.warcMovedToValid(validWarcDirectory, warcFilename)) {
+                            logger.info(warcFilename + " already validated and moved to final directory");
+                        } else {
 
-                            // har rapport fra før, sjekker status i rapport
-                            if (service.warcStatusIsValidAndWellFormed(validationReport)) {
-                                logger.info(warcFilename +
-                                        " , status: Well-formed and valid. Moving WARC to final directory");
-                                service.copyWarcToValidWarcsFolder(warc,
-                                        new File(validWarcDirectory + warcFilename));
+                            // Sjekker om warc-fil har en rapport
+                            File validationReport = service.reportForWarcExist(reports, warcFilename);
+                            if (validationReport != null) {
+
+                                // har rapport fra før, sjekker status i rapport
+                                if (service.warcStatusIsValidAndWellFormed(validationReport)) {
+                                    logger.info(warcFilename +
+                                            " , status: Well-formed and valid. Moving WARC to final directory");
+                                    service.copyWarcToValidWarcsFolder(warc,
+                                            new File(validWarcDirectory + warcFilename));
+                                } else {
+                                    logger.info("WARC: " + warcFilename + " contains errors, will not be moved");
+                                }
+
                             } else {
-                                logger.info("WARC: " + warcFilename + " contains errors, will not be moved");
+                                logger.info("Can't find a validation report for: " + warcFilename);
+                                reportName = warcFilePath + ".xml";
+                                logger.info("Will create report using Jhove, with name: " + reportName);
+                                service.validateWarc(warcFilePath, reportName);
+                                files = contentDirectory.listFiles();
+                                reports = service.findAllReports(files);
                             }
 
-                        } else {
-                            logger.info("Can't find a validation report for: " + warcFilename);
-                            reportName = warcFilePath + ".xml";
-                            logger.info("Will create report using Jhove, with name: " + reportName);
-                            service.validateWarc(warcFilePath, reportName);
-                            files = contentDirectory.listFiles();
-                            reports = service.findAllReports(files);
                         }
-
                     }
+                }else{
+                    logger.info("No files in directory to check.");
                 }
                 try {
                     logger.info("Thread will sleep for: " + sleepBetweenChecks + " seconds");
